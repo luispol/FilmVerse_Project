eda2c388a9b5902e2ebe3e5f3d2e1207fd1dad4e
JorgeMendezV <jorgemendez3421@gmail.com>
2023-05-17 15:14:09 -0600

    Movie card information completed

--- js/main.js
+++ js/main.js
@@ -20,3 +20,24 @@ var swiper = new Swiper(".home", {
       clickable: true,  // Permitir hacer clic en la paginación para navegar a una diapositiva específica
     }
   });
+
+
+// LOGICA PARA BOTÓN WATCH TRAILER
+// Obtiene el elemento del botón "Watch Trailer" por su ID
+const watchTrailerBtn = document.getElementById('watch-trailer-btn');
+
+// Obtiene el contenedor del video por su ID
+const videoContainer = document.getElementById('video-container');
+
+// Agrega un event listener al botón "Watch Trailer" que se activa cuando se hace clic en él
+watchTrailerBtn.addEventListener('click', () => {
+  // Muestra el contenedor del video estableciendo su estilo display como "flex"
+  videoContainer.style.display = 'flex';
+});
+
+// Agrega un event listener al contenedor del video que se activa cuando se hace clic en él
+videoContainer.addEventListener('click', () => {
+  // Oculta el contenedor del video estableciendo su estilo display como "none"
+  videoContainer.style.display = 'none';
+});
+
