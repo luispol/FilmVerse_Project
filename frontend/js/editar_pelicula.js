8a99849cf131949de07d2543bd7fa298c5cfe5ee
luispol <lumpolanco@gmail.com>
2023-06-01 00:07:39 -0600

    two change more

--- frontend/js/editar_pelicula.js
+++ frontend/js/editar_pelicula.js
@@ -1,6 +1,6 @@
 //Variables
 const formNuevaPelicula = document.querySelector("#formNuevaPelicula")
-let token
+let idMovie
 
 //Eventos
 document.addEventListener("DOMContentLoaded",cargarDatos)
@@ -8,24 +8,13 @@ formNuevaPelicula.addEventListener("submit",guardar)
 
 //Funciones
 function cargarDatos(){
-    token = localStorage.getItem("token")
-    if (!token){
-        location.href="login.html"
+    idMovie = localStorage.getItem("id")
+    if (!idMovie){
+        location.href="admin_view.html"
     }
 }
 
 function guardar(event){
     event.preventDefault()
-    var raw = "";
 
-    var requestOptions = {
-    method: 'PUT',
-    body: raw,
-    redirect: 'follow'
-    };
-
-    fetch("localhost:3001/api/movies/card?id=6476f5eed04367b90ee7f007", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
 }
